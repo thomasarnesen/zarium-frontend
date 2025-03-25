aa6e5ad2e7b09deac01dd39b8513bf3b73621cb0
Thomas Arnesen <thomas-arnesen@outlook.com>
2025-03-25 12:22:17 +0100

    x

--- postcss.config.js
+++ postcss.config.js
@@ -1,16 +1,6 @@
-module.exports = {
+export default {
   plugins: {
     tailwindcss: {},
-    autoprefixer: {
-      overrideBrowserslist: [
-        "defaults",
-        "not IE 11",
-        "> 1%",
-        "last 2 versions",
-        "Firefox ESR",
-        "Safari >= 10"
-      ],
-      grid: true
-    },
+    autoprefixer: {},
   },
-}
+};
